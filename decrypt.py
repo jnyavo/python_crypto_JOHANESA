17785bbcbeb328260075912448dd26be5d97b3f2
nyavo <jnyavo@gmail.com>
2020-09-16 20:59:43 +0000

    Variables positionnelles : in, out, clé privé, clé public

--- decrypt.py
+++ decrypt.py
@@ -3,19 +3,59 @@ import sys
 from Crypto.PublicKey import RSA
 
 
-#Ouverture des fichiers PEM
+fichier_in = False
+fichier_out = False
+cle_prive = False
+cle_public = False
 
-with open('private.pem','r') as fk:
-	private = RSA.importKey(fk.read())
-	fk.close()
-
-with open('public.pem', 'r') as fp:
-	public = RSA.importKey(fp.read())
-	fp.close()
-
+chemin_gen = "./keys/"
+private_rsa_file = "private.pem"
+public_rsa_file = "public.pem"
 
 
-if(len(sys.argv) > 1):
+
+
+if (len(sys.argv) > 1):
+        fichier_in = True
+if (len(sys.argv) > 2):
+        fichier_out = True
+if (len(sys.argv) > 3):
+        cle_prive = True
+if (len(sys.argv) > 4):
+        cle_public = True
+
+
+
+def get_keys(private,public):
+
+        #Prend les clés
+        with open(private,'r') as fk:
+                private = RSA.importKey(fk.read())
+                fk.close()
+
+        with open(public, 'r') as fp:
+                public = RSA.importKey(fp.read())
+                fp.close()
+
+        return (private,public)
+
+
+
+
+#Importation des clés
+
+
+if(cle_prive and cle_public):
+
+        #importer clé existant
+        try:
+                key = get_keys(sys.argv[3],sys.argv[4])
+        except:
+                key = get_keys(chemin_gen+sys.argv[3],chemin_gen+sys.argv[4])
+else:
+	key = get_keys(chemin_gen+private_rsa_file,chemin_gen+private_rsa_file)
+
+if(fichier_in):
         fichier = sys.argv[1]
 else:
         fichier = input("Le fichier à crypter: ")
@@ -25,14 +65,14 @@ else:
 with open(fichier, "rb") as fr:
 	d_encrypt = fr.read()
 	fr.close()
-x = private.decrypt(d_encrypt)
+x = key[0].decrypt(d_encrypt)
 
 
 
 #Sauvegarde du fichier decrypté
 
-output = fichier + '.crypt'
-if (len(sys.argv) > 2):
+output = fichier + '.decrypt'
+if (fichier_out):
         output = sys.argv[2]
 
 with open(output,"wb") as fd:
--- encrypt.py
+++ encrypt.py
@@ -1,44 +1,108 @@
 #!/usr/bin/env python3
 import sys
+import os
 from Crypto.PublicKey import RSA
 
-#Génerer la clé
 
-key = RSA.generate(1024)
+fichier_in = False
+fichier_out = False
+cle_prive = False
+cle_public = False
 
-
-#Exporter la clé dans d'autres variables
-
-k = key.exportKey("PEM")
-
-p = key.publickey().exportKey('PEM')
-
-
-#Ecrire les clés dans des fichiers pem
-
-with open('private.pem','w') as kf:
-	kf.write(k.decode())
-	kf.close()
-
-with open('public.pem','w') as pf:
-	pf.write(p.decode())
-	pf.close()
+chemin_gen = "./keys/"
+private_rsa_file = "private.pem"
+public_rsa_file = "public.pem"
 
 
-#Encryption
-if(len(sys.argv) > 1):
+
+
+if (len(sys.argv) > 1):
+	fichier_in = True
+if (len(sys.argv) > 2):
+	fichier_out = True
+if (len(sys.argv) > 3):
+	cle_prive = True
+if (len(sys.argv) > 4):
+	cle_public = True 
+
+
+
+def get_keys(private,public):
+
+	#Prend les clés
+	with open(private,'r') as fk:
+        	private = RSA.importKey(fk.read())
+        	fk.close()
+
+	with open(public, 'r') as fp:
+        	public = RSA.importKey(fp.read())
+        	fp.close()
+
+	return (private,public)
+
+
+
+def gen_rsaFiles(private,public):
+
+	#Génerer la clé
+	
+	key = RSA.generate(1024)
+	
+
+	#Exporter la clé dans d'autres variables
+
+	k = key.exportKey("PEM")
+
+	p = key.publickey().exportKey('PEM')
+
+	
+	#Ecrire les clés dans des fichiers pem
+	
+	os.system("mkdir keys" )
+	
+	with open(private,'w') as kf:
+		kf.write(k.decode())
+		kf.close()
+
+	with open(public,'w') as pf:
+		pf.write(p.decode())
+		pf.close()
+
+	return key
+
+
+#Determination de clé
+
+if(cle_prive and cle_public):
+
+	#importer clé existant
+	try:
+		key = get_keys(sys.argv[3],sys.argv[4])[0]
+	except:
+		key = get_keys(chemin_gen+sys.argv[3],chemin_gen+sys.argv[4])[0]
+else:
+	#generer nouveau clé
+	key = gen_rsaFiles(chemin_gen+private_rsa_file,chemin_gen+public_rsa_file)
+
+
+#Ouverture du fichier
+
+if(fichier_in):
 	fichier = sys.argv[1]
 else:
 	fichier = input("Le fichier à crypter: ")
 
+
 fo = open(fichier,"rb")
 text = fo.read()
-d_encrypt = key.publickey().encrypt(text,32)
+
+#Encryption
+d_encrypt = key.publickey().encrypt(text,None)
 
 #Ecriture du fichier encrypté
 
 output = fichier + '.crypt'
-if (len(sys.argv) > 2):
+if (fichier_out):
 	output = sys.argv[2]
 
 fo = open(output, 'wb')
